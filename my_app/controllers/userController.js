205f3525c29d148f1f3681014c89c4853bd003fd
TheWolek <pawelm916@wp.pl>
2020-07-07 21:09:22 +0200

    sign in form handling

--- my_app/controllers/userController.js
+++ my_app/controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.user_detail = function (req, res) {
     res.send('in progress: user detail: ' + req.params.id);
 };
@@ -18,6 +20,22 @@ exports.user_login_get = function (req, res) {
     res.render("user", {});
 };
 
-exports.user_login_post = function (req, res) {
-    res.send('in progress: user login POST');
+exports.user_login_post = function (req, res, next) {
+    console.log(req.body)
+    let mail = req.body.login
+    let pass = req.body.pass
+
+    User.find({ email: mail, password: pass })
+        .exec(function (err, foundUser) {
+            if (err) return next(err)
+
+            if (foundUser.length == 0) {
+                let err = new Error('zły e-mail lub hasło')
+                return next(err)
+            } else {
+                console.log(foundUser)
+                res.send('in progress: user login POST');
+            }
+        })
+
 };
